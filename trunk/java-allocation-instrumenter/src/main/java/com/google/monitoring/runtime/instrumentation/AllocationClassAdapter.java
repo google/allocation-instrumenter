6c0532b8074e4231a525a38b08e1aced572b0cfd
jeremy.manson@gmail.com <jeremy.manson@gmail.com@0cf7ad32-d877-11de-87f0-6f0b95ab10f2>
2012-01-31 21:01:32 +0000

    Major feature integration.
    1) Change allocation instrumenter to compute stackmaps.  We previously 
    output broken stackmaps, but verifiers prior to the JDK7 Hotspot didn't care.
    
    Computing stackmaps using ASM requires computation of the nearest
    superclass for a pair of classes.  The problem is that some of these
    superclasses may not be loaded yet.  ASM assumes that all necessary
    classes have been loaded.  As a result, we need a version of ASM's
    ClassWriter that grovels through the classpath for superclasses,
    where necessary.
    
    2) Add the ability to instrument the constructors of individual classes.

---
+++ trunk/java-allocation-instrumenter/src/main/java/com/google/monitoring/runtime/instrumentation/AllocationClassAdapter.java
@@ -0,0 +1,56 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+
+package com.google.monitoring.runtime.instrumentation;
+
+import org.objectweb.asm.ClassAdapter;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.commons.LocalVariablesSorter;
+import org.objectweb.asm.commons.JSRInlinerAdapter;
+
+/**
+ * Instruments bytecodes that allocate heap memory to call a recording hook.
+ * A <code>ClassAdapter</code> that processes methods with a
+ * <code>AllocationMethodAdapter</code> to instrument heap allocations.
+ *
+ * @author 
+ * @author fischman@google.com (Ami Fischman) (Original Author)
+ */
+class AllocationClassAdapter extends ClassAdapter {
+  private final String recorderClass;
+  private final String recorderMethod;
+
+  public AllocationClassAdapter(ClassVisitor cv, String recorderClass,
+      String recorderMethod) {
+    super(cv);
+    this.recorderClass = recorderClass;
+    this.recorderMethod = recorderMethod;
+  }
+
+  /**
+   * For each method in the class being instrumented, <code>visitMethod</code>
+   * is called and the returned MethodVisitor is used to visit the method.
+   * Note that a new MethodVisitor is constructed for each method.
+   */
+  @Override
+  public MethodVisitor visitMethod(int access, String base, String desc,
+      String signature, String[] exceptions) {
+    MethodVisitor mv =
+      cv.visitMethod(access, base, desc, signature, exceptions);
+
+    if (mv != null) {
+      // We need to compute stackmaps (see
+      // AllocationInstrumenter#instrument).  This can't really be
+      // done for old bytecode that contains JSR and RET instructions.
+      // So, we remove JSRs and RETs.
+      JSRInlinerAdapter jsria = new JSRInlinerAdapter(
+          mv, access, base, desc, signature, exceptions);
+      AllocationMethodAdapter aimv =
+        new AllocationMethodAdapter(jsria, recorderClass, recorderMethod);
+      LocalVariablesSorter lvs = new LocalVariablesSorter(access, desc, aimv);
+      aimv.lvs = lvs;
+      mv = lvs;
+    }
+    return mv;
+  }
+}
