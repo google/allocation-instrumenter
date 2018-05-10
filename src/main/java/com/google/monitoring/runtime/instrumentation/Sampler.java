697a02cd0741ab107a656695936f06ecd63540bc
Jeremy Manson <jeremymanson@google.com>
2018-05-10 09:48:43 -0700

    Reformat code with http://github.com/google/google-java-format

--- src/main/java/com/google/monitoring/runtime/instrumentation/Sampler.java
+++ src/main/java/com/google/monitoring/runtime/instrumentation/Sampler.java
@@ -16,30 +16,22 @@
 
 package com.google.monitoring.runtime.instrumentation;
 
-/**
- * This interface describes a function that is used to sample an allocation. 
- * 
- */
+/** This interface describes a function that is used to sample an allocation. */
 public interface Sampler {
   /**
-   * Determines whether the object currently being allocated, with the given
-   * size, should be traced.
-   * 
-   * <b>CAUTION: DO NOT DO ALLOCATION IN THIS METHOD WITHOUT ENSURING THAT
-   * THE SAMPLER WILL NOT BE INVOKED ON THE RESULTING ALLOCATION.</b>
-   * Otherwise, you will get an infinite regress of calls to the sampler.
-   * 
-   * @param count the <code>int</code> count of how many instances are being
-   *     allocated.  -1 means a simple new to distinguish from a 1-element array.  0
-   *     shows up as a value here sometimes; one reason is T[] toArray()-type
-   *     methods that require an array type argument (see ArrayList.toArray() for
-   *     example).
-   * @param desc the <code>String</code> descriptor of the class/primitive type
-   *     being allocated.
-   * @param newObj the new <code>Object</code> whose allocation we're
-   *     recording.
+   * Determines whether the object currently being allocated, with the given size, should be traced.
+   *
+   * <p><b>CAUTION: DO NOT DO ALLOCATION IN THIS METHOD WITHOUT ENSURING THAT THE SAMPLER WILL NOT
+   * BE INVOKED ON THE RESULTING ALLOCATION.</b> Otherwise, you will get an infinite regress of
+   * calls to the sampler.
+   *
+   * @param count the <code>int</code> count of how many instances are being allocated. -1 means a
+   *     simple new to distinguish from a 1-element array. 0 shows up as a value here sometimes; one
+   *     reason is T[] toArray()-type methods that require an array type argument (see
+   *     ArrayList.toArray() for example).
+   * @param desc the <code>String</code> descriptor of the class/primitive type being allocated.
+   * @param newObj the new <code>Object</code> whose allocation we're recording.
    * @param size the size of the object being allocated.
    */
-  public void sampleAllocation(int count, String desc,
-      Object newObj, long size);
+  public void sampleAllocation(int count, String desc, Object newObj, long size);
 }
--- src/main/java/com/google/monitoring/runtime/instrumentation/VerifyingClassAdapter.java
+++ src/main/java/com/google/monitoring/runtime/instrumentation/VerifyingClassAdapter.java
@@ -25,38 +25,34 @@ import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.commons.CodeSizeEvaluator;
 
 /**
- * This is a class writer that gets used in place of the existing 
- * {@link org.objectweb.asm.ClassWriter},
- * and verifies properties of the class getting written.
- * 
- * Currently, it only checks to see if the methods are of the correct length
- * for Java methods (&lt;64K).
- * 
+ * This is a class writer that gets used in place of the existing {@link
+ * org.objectweb.asm.ClassWriter}, and verifies properties of the class getting written.
+ *
+ * <p>Currently, it only checks to see if the methods are of the correct length for Java methods
+ * (&lt;64K).
  */
 public class VerifyingClassAdapter extends ClassVisitor {
-  private static final Logger logger =
-    Logger.getLogger(VerifyingClassAdapter.class.getName());
+  private static final Logger logger = Logger.getLogger(VerifyingClassAdapter.class.getName());
 
-  /**
-   * An enum which indicates whether the class in question is verified.
-   */
+  /** An enum which indicates whether the class in question is verified. */
   public enum State {
-    PASS, UNKNOWN, FAIL_TOO_LONG;
+    PASS,
+    UNKNOWN,
+    FAIL_TOO_LONG;
   }
-  
+
   final ClassWriter cw;
-  final byte [] original;
+  final byte[] original;
   final String className;
   String message;
   State state;
-  
+
   /**
    * @param cw A class writer that is wrapped by this class adapter
    * @param original the original bytecode
    * @param className the name of the class being examined.
    */
-  public VerifyingClassAdapter(ClassWriter cw, byte [] original,
-      String className) {
+  public VerifyingClassAdapter(ClassWriter cw, byte[] original, String className) {
     super(Opcodes.ASM6, cw);
     state = State.UNKNOWN;
     message = "The class has not finished being examined";
@@ -67,9 +63,9 @@ public class VerifyingClassAdapter extends ClassVisitor {
 
   /**
    * {@inheritDoc}
-   * 
-   * In addition, the returned {@link org.objectweb.asm.MethodVisitor}
-   * will throw an exception if the method is greater than 64K in length.
+   *
+   * <p>In addition, the returned {@link org.objectweb.asm.MethodVisitor} will throw an exception if
+   * the method is greater than 64K in length.
    */
   @Override
   public MethodVisitor visitMethod(
@@ -78,8 +74,7 @@ public class VerifyingClassAdapter extends ClassVisitor {
       final String desc,
       final String signature,
       final String[] exceptions) {
-    MethodVisitor mv =
-      super.visitMethod(access, name, desc, signature, exceptions);
+    MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
     return new CodeSizeEvaluator(mv) {
       @Override
       public void visitEnd() {
@@ -91,10 +86,8 @@ public class VerifyingClassAdapter extends ClassVisitor {
       }
     };
   }
-  
-  /**
-   * {@inheritDoc}
-   */
+
+  /** {@inheritDoc} */
   @Override
   public void visitEnd() {
     super.visitEnd();
@@ -105,22 +98,21 @@ public class VerifyingClassAdapter extends ClassVisitor {
 
   /**
    * Gets the verification state of this class.
-   * 
+   *
    * @return true iff the class passed inspection.
    */
   public boolean isVerified() {
     return state == State.PASS;
   }
-  
+
   /**
    * Returns the byte array that contains the byte code for this class.
-   * 
+   *
    * @return a byte array.
    */
   public byte[] toByteArray() {
     if (state != State.PASS) {
-      logger.log(Level.WARNING,
-          "Failed to instrument class " + className + " because " + message);
+      logger.log(Level.WARNING, "Failed to instrument class " + className + " because " + message);
       return original;
     }
     return cw.toByteArray();
